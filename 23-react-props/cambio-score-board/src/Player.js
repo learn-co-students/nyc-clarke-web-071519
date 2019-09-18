4bcd64486799a0b9f4f6f839928625de5f9c213f
= <=>
2019-09-18 12:30:09 -0400

    Add lecture notes

--- 23-react-props/cambio-score-board/src/Player.js
+++ 23-react-props/cambio-score-board/src/Player.js
@@ -1,74 +1,52 @@
-import React, { Component } from 'react'
-
-
-const styles = {
-  border: '1px solid black',
-  display: 'flex',
-  alignItems: 'center',
-  padding: '0.5rem',
-  margin: '0.5rem 0',
-
-  profile: {
-    fontSize: '14px',
-    width: '100px',
-    display: 'flex',
-    flexDirection: 'column',
-    height: '140px',
-    justifyContent: 'space-between',
-    textAlign: 'center',
-    alignItems: 'center'
-  },
-  img: {
-    width: '80px',
-    borderRadius: '50%'
-  }
-}
+import React, { Component } from 'react';
+import './Player.css';
 
 class Player extends Component {
 
   renderRounds() {
-    return this.props.player.rounds.map((score, idx) => {
+    const { rounds } = this.props.player;
+    return rounds.map((score, idx) => {
       return (
-        <div style={styles.profile}>
-          <p style={{ fontSize: '64px', margin: '0', width: '80px', height: '80px' }}>{score}</p>
-          <p className="player-total">Round {idx + 1}</p>
+        <div key={idx} className="score-card">
+          <p className="score">{score}</p>
+          <p className="player-total">Round {rounds.length - idx}</p>
         </div>
       )
     })
 
   }
 
-  handleSubmit= e => {
-    e.preventDefault()
+  handleSubmit = e => {
+    e.preventDefault();
 
-    const playerId = this.props.player.id
-    const score = e.target.score.value
+    const playerId = this.props.player.id;
+    const score = e.target.score.value;
 
-    this.props.addRoundToPlayer(playerId, score)
+    this.props.addRoundToPlayer(playerId, score);
   }
 
   render() {
     const { name, total, img_url } = this.props.player;
     return (
-      <div style={styles} className="player-card">
-        <div style={styles.profile} className="profile">
-          <img style={styles.img} src={img_url} alt=""/>
+      <div className="player-card">
+        <div className="profile">
+          <img src={img_url} alt="" />
           <p className="player-name">{name}</p>
         </div>
-
-        <div style={styles.profile} className="player-total">
-          <p style={{fontSize: '64px', margin: '0', width: '80px', height: '80px'}}>{total}</p>
+        
+        <div className="score-card total">
+          <p style={{ fontSize: '50px' }} className="score">{total}</p>
           <p className="player-total">Total</p>
         </div>
 
-        { this.renderRounds() }
+        <div className="rounds">
+          {this.renderRounds()}
+        </div>
 
-        <div className="add-score">
         <form onSubmit={this.handleSubmit}>
-          <input name="score" style={{width: '50px'}} type="number" />
+          <input name="score" type="number" placeholder="score" />
           <button type="submit">Add Score</button>
         </form>
-        </div>
       </div>
     )
   }
--- 23-react-props/cambio-score-board/src/PlayerList.js
+++ 23-react-props/cambio-score-board/src/PlayerList.js
@@ -1,90 +1,23 @@
-import React, { Component } from 'react'
-import Player from './Player'
+import React, { Component } from 'react';
+import Player from './Player';
 
 const styles = {
   display: 'flex',
   flexDirection: 'column',
-  border: '1px solid black',
 }
 
 class PlayerList extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      players: [
-        { id: 1,
-          name: 'Greg (The Great Cambino)',
-          rounds: [],
-          total: 0,
-          img_url: 'https://shreckhiseshrubbery.com/wp-content/uploads/2018/01/placeholder-pic.png'
-        },
-        { id: 2,
-          name: 'Meghann (The Original)',
-          rounds: [],
-          total: 0,
-          img_url: 'https://shreckhiseshrubbery.com/wp-content/uploads/2018/01/placeholder-pic.png'
-        },
-        { id: 3,
-          name: 'Alyssa (No.)',
-          rounds: [],
-          total: 0,
-          img_url: 'https://shreckhiseshrubbery.com/wp-content/uploads/2018/01/placeholder-pic.png'
-        },
-        { id: 4,
-          name: 'Val',
-          rounds: [],
-          total: 0,
-          img_url: 'https://shreckhiseshrubbery.com/wp-content/uploads/2018/01/placeholder-pic.png'
-        },
-        { id: 5,
-          name: 'Varun (The Penalizer)',
-          rounds: [],
-          total: 23,
-          img_url: 'https://shreckhiseshrubbery.com/wp-content/uploads/2018/01/placeholder-pic.png'
-        },
-        { id: 6,
-          name: 'Andrew (The 50 Master)',
-          rounds: [],
-          total: 0,
-          img_url: 'https://shreckhiseshrubbery.com/wp-content/uploads/2018/01/placeholder-pic.png'
-        },
-        { id: 7,
-          name: 'AJ (The Great 1)',
-          rounds: [],
-          total: 0,
-          img_url: 'https://shreckhiseshrubbery.com/wp-content/uploads/2018/01/placeholder-pic.png'
-        },
-        { id: 8,
-          name: 'Hyo (Gotta be Quicker than that)',
-          rounds: [],
-          total: 0,
-          img_url: 'https://shreckhiseshrubbery.com/wp-content/uploads/2018/01/placeholder-pic.png'
-        }
-      ]
-    }
-  }
-
-  addRoundToPlayer = (id, score) => {
-    const newPlayers = this.state.players.map( player => {
-      if (player.id === id) {
-        const playerCopy = { ...player }
-        playerCopy.rounds.push(score)
-        return playerCopy
-      } else {
-        return player
-      }
-    })
-    this.setState({ players: newPlayers })
-  }
 
   render() {
     return (
       <div style={styles} className="player-list">
       {
-        this.state.players.map(player => <Player 
-          addRoundToPlayer={this.addRoundToPlayer} 
-          player={player} />
+        this.props.players.map(player => 
+          <Player
+            key={player.id}
+            addRoundToPlayer={this.props.addRoundToPlayer} 
+            player={player} 
+          />
         )
       }
       </div>
